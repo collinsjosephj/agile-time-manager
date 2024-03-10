6fffe9969a29e37def4e84359081bb7db2efc615
Joseph "Jessy" Collins <156174614+collinsjosephj@users.noreply.github.com>
2024-03-09 23:57:10 -0800

    fixed names of some variables

--- Develop/script.js
+++ Develop/script.js
@@ -1,8 +1,7 @@
 // Wrap all code that interacts with the DOM in a call to jQuery to ensure that
 // the code isn't run until the browser has finished rendering all the elements
 // in the html.
-$(function () {
-  // TODO: Add a listener for click events on the save button. This code should
+// TODO: Add a listener for click events on the save button. This code should
   // use the id in the containing time-block as a key to save the user input in
   // local storage. HINT: What does `this` reference in the click listener
   // function? How can DOM traversal be used to get the "hour-x" id of the
@@ -20,4 +19,14 @@ $(function () {
   // attribute of each time-block be used to do this?
   //
   // TODO: Add code to display the current date in the header of the page.
+  //const hours = ['9', '10', '11', '12', '13', '14', '15', '16', '17'] lll
+
+const localeSet = {};
+dayjs.locale(localeSet);
+// Will wait until the DOM is fully loaded before executing the function
+$(function () {
+// Function will get the current hour of the day -- referenced from the dayjs library String + Format
+const thisHour = dayjs().format('H');
+// 'H' denotes a 24 hour clock to be called from the dayjs library 
 });
+
