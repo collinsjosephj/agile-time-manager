6cd0706f3932a89ea737e6e4ef29ca2b3c62913c
Joseph "Jessy" Collins <156174614+collinsjosephj@users.noreply.github.com>
2024-03-10 03:32:50 -0700

    troubleshooting updateTime

--- Develop/script.js
+++ Develop/script.js
@@ -19,45 +19,66 @@
   // attribute of each time-block be used to do this?
   //
   // TODO: Add code to display the current date in the header of the page.
-  //const hours = ['9', '10', '11', '12', '13', '14', '15', '16', '17'] 
 
-const localeSet = {};
+//const dayjs = require("dayjs");
+
+const localeSet = {}; // redundant, maybe comment this out!!!!!!!!
 dayjs.locale(localeSet);
 // Will wait until the DOM is fully loaded before executing the function
-$(function () {
-// Function will get the current hour of the day -- referenced from the dayjs library String + Format
-const thisHour = dayjs().format('H');
-// 'H' denotes a 24 hour clock to be called from the dayjs library
-// Function below toggles the ".time-block" based on the "past", "present", and "future" class relative to my thisHour variable. 
-function hourColor() {
-  $('.time-block').forEach(function() {
-    const timeBlockHour = parseInt(this.id);
-    $(this).toggleClass('past', timeBlockHour < thisHour );
-    $(this).toggleClass('present', timeBlockHour === thisHour );
-    $(this).toggleClass('future', timeBlockHour > thisHour );
-  });
-}
-// eventLister included in bottom function to save user input to localStorage
-function saveToLocal() {
-  $('.saveBtn').on('click', function() {
-    const keyInput = $(this).parent().attr('id');
-    const valueInput = $(this).siblings('.description').val();
-    localStorage.setItem(keyInput, valueInput);
-  });
-}
-// Function below will refresh each ".time-block" relative to the thisHour variable whether it is "past"= gray, "present"=red, or the "future"=green. 
-// Function will also be built to  pull user input from localStorage and set the textarea values for each corresponding ".time-block"
-function changeColor() {
-  $('.time-block').forEach
+$(function() {
+  // Function will get the current hour of the day -- referenced from the dayjs library String + Format
+  const thisHour = dayjs().format('H'); // 'H' denotes a 24 hour clock to be called from the dayjs library (works for me, as I work on a 24 hour clock)
+  // Function below toggles the ".time-block" based on the "past", "present", and "future" class relative to my thisHour variable.
+  // .addClass & .removeClass didnt make conceptual sense to me here, so I found .toggleClass on https://htmlcheatsheet.com/jquery/
+  function hourColor() {
+    $('.time-block').each(function() {
+      const timeBlockHour = parseInt(this.id);
+      $(this).toggleClass('past', timeBlockHour < thisHour );
+      $(this).toggleClass('present', timeBlockHour === thisHour );
+      $(this).toggleClass('future', timeBlockHour > thisHour );
+    });
+  }
+  // eventLister included in bottom function to save user input to localStorage
+  function saveToLocal() {
+    $('.saveBtn').on('click', function() {
+      const keyInput = $(this).parent().attr('id');
+      const valueInput = $(this).siblings('.description').val();
+      localStorage.setItem(keyInput, valueInput);
+    });
+  }
+  // Function below will refresh each ".time-block" relative to the thisHour variable whether it is past= gray, present=red, or the future=green. 
+  // Function will also be built to pull user input from localStorage and set the textarea values for each corresponding ".time-block"
+  
+  function changeColor() {
+    $('.time-block').each(function() {
+      const timeBlockHour = parseInt(this.id);
+      if ( timeBlockHour == thisHour ) {
+        $(this).removeClass('past', 'future').addClass('present');
+      } else if ( timeBlockHour < thisHour ) {
+        $(this).removeClass('present', 'future').addClass('past');
+      } else ( timeBlockHour > thisHour )
+        $(this).removeClass('past', 'present').addClass('future');
+    });
+    $('time.block').each(function() {
+      const keyInput = $(this).attr('id');
+      const valueInput = localStorage.getItem(keyInput);
+      $(this).siblings('.description').val();
+    });
+  }
 
-}
-
-
-
-
-
-
-hourColor();
-saveToLocal();
-}); // this one is for the overarching func :)
+  // Function below will display the time at the top of the application. 
+  function updateTime() {
+    const dateEl = $('#date');
+    const timeEl = $('#time');
+    const dateNow = dayjs().format('MMM D, YYYY');
+    const timeNow = dayjs().format('hh:mm:ss');
+    dateElement.text(dateNow);
+    timeElement.text(timeNow);
+  }
+  
+  hourColor();
+  saveToLocal();
+  changeColor();
+  updateTime(timeNow, 1000); // this one is for the overarching func
+});
 
