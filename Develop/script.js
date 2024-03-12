bdc9cf0693b93a4849efdb8f9fdf51eaf3e0e9bb
Joseph "Jessy" Collins <156174614+collinsjosephj@users.noreply.github.com>
2024-03-12 01:07:11 -0700

    troubleshooting display of hours 9-5

--- Develop/script.js
+++ Develop/script.js
@@ -21,24 +21,23 @@
   // TODO: Add code to display the current date in the header of the page.
 
 //const dayjs = require("dayjs");
-const hours = ['9', '10', '11', '12', '13', '14', '15', '16', '17']
+
 
 const localeSet = {}; // redundant, maybe comment this out!!!!!!!!
 dayjs.locale(localeSet);
 // Will wait until the DOM is fully loaded before executing the function
 $(function() {
   // Function will get the current hour of the day -- referenced from the dayjs library String + Format
-  // 'H' denotes a 24 hour clock to be called from the dayjs library (works for me, as I work on a 24 hour clock)
+  // 'H' denotes a 24 hour clock to be called from the dayjs library 
   const thisHour = dayjs().format('H'); 
-  console.log(thisHour);
   // Function below toggles the ".time-block" based on the "past", "present", and "future" class relative to my thisHour variable.
   // .addClass & .removeClass didnt make conceptual sense to me here, so I found .toggleClass on https://htmlcheatsheet.com/jquery/
   function hourColor() {
     $('.time-block').each(function() {
-      const timeBlockHour = parseInt(this.id);
-      $(this).toggleClass('past', timeBlockHour < thisHour );
-      $(this).toggleClass('present', timeBlockHour === thisHour );
-      $(this).toggleClass('future', timeBlockHour > thisHour );
+      const hourBlock = parseInt(this.id);
+      $(this).toggleClass('past', hourBlock < thisHour );
+      $(this).toggleClass('present', hourBlock === thisHour );
+      $(this).toggleClass('future', hourBlock > thisHour );
     });
   }
   // eventLister included in bottom function to save user input to localStorage
@@ -53,12 +52,12 @@ $(function() {
   
   function changeColor() {
     $('.time-block').each(function() {
-      const timeBlockHour = parseInt(this.id);
-      if ( timeBlockHour == thisHour ) {
+      const hourBlock = parseInt(this.id);
+      if ( hourBlock == thisHour ) {
         $(this).removeClass('past', 'future').addClass('present');
-      } else if ( timeBlockHour < thisHour ) {
+      } else if ( hourBlock < thisHour ) {
         $(this).removeClass('present', 'future').addClass('past');
-      } else ( timeBlockHour > thisHour )
+      } else ( hourBlock > thisHour )
         $(this).removeClass('past', 'present').addClass('future');
     });
   // Function will also be built to pull user input from localStorage and set the textarea values for each corresponding ".time-block"
@@ -80,19 +79,15 @@ $(function() {
     dateElement.text(dateNow);
     timeElement.text(timeNow);
   }
-  function newRow() {
-    var rowElement = $('.container-lg');
-    const content = `
-    <div id="hour-12" class="row time-block past">
-    <div class="col-2 col-md-1 hour text-center py-3">9AM</div>
-    <textarea class="col-8 col-md-10 description" rows="3"> </textarea>
-    <button class="btn saveBtn col-2 col-md-1" aria-label="save">
-      <i class="fas fa-save" aria-hidden="true"></i>
-    </button>
-    </div>
-    `;
-    rowElement.html(content);
-  }
+  /*$(document).ready(function() {
+    const container = $('.container-lg');
+    const hours = ['09', '10', '11', '12', '13', '14', '15', '16', '17']; //24 hour time clock
+
+    hours.forEach(hour => {
+      const hourId = 
+
+    });*/
+  
   hourColor();
   saveToLocal();
   changeColor();
